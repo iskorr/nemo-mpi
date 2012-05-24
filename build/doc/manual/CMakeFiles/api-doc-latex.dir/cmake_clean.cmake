358587d7d0bfde3ecaf7328f6c96b786c941758a
Iskander Orazbekov <io1009@imperial.ac.uk>
2012-05-24 20:06:31 +0100

    Deleted build

--- build/doc/manual/CMakeFiles/api-doc-latex.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/api-doc-latex"
-  "manual.pdf"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/api-doc-latex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
