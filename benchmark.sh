c583724c6082cec80d1349aa65595d3559356175
Iskander Orazbekov <io1009@imperial.ac.uk>
2012-06-15 14:18:52 +0100

    Set of benchmarks

--- benchmark.sh
+++ benchmark.sh
@@ -1,42 +1,42 @@
-./random output/rand.txt -n 1000 -m 300
-./random output/rand.txt -n 2000 -m 300
-./random output/rand.txt -n 5000 -m 300
-./random output/rand.txt -n 10000 -m 300
-./random output/rand.txt -n 20000 -m 300
-./random output/rand.txt -n 50000 -m 300
-./random output/rand.txt -n 100000 -m 300
-mpirun -np 2 ./mpi_dist_general 1000 300 0 500 1 0 output/mpi1.txt
-mpirun -np 3 ./mpi_dist_general 1000 300 0 500 1 0 output/mpi2.txt
-mpirun -np 4 ./mpi_dist_general 1000 300 0 500 1 0 output/mpi3.txt
-mpirun -np 5 ./mpi_dist_general 1000 300 0 500 1 0 output/mpi4.txt
-mpirun -np 6 ./mpi_dist_general 1000 300 0 500 1 0 output/mpi5.txt
-mpirun -np 2 ./mpi_dist_general 2000 300 0 500 1 0 output/mpi1.txt
-mpirun -np 3 ./mpi_dist_general 2000 300 0 500 1 0 output/mpi2.txt
-mpirun -np 4 ./mpi_dist_general 2000 300 0 500 1 0 output/mpi3.txt
-mpirun -np 5 ./mpi_dist_general 2000 300 0 500 1 0 output/mpi4.txt
-mpirun -np 6 ./mpi_dist_general 2000 300 0 500 1 0 output/mpi5.txt
-mpirun -np 2 ./mpi_dist_general 5000 300 0 500 1 0 output/mpi1.txt
-mpirun -np 3 ./mpi_dist_general 5000 300 0 500 1 0 output/mpi2.txt
-mpirun -np 4 ./mpi_dist_general 5000 300 0 500 1 0 output/mpi3.txt
-mpirun -np 5 ./mpi_dist_general 5000 300 0 500 1 0 output/mpi4.txt
-mpirun -np 6 ./mpi_dist_general 5000 300 0 500 1 0 output/mpi5.txt
-mpirun -np 2 ./mpi_dist_general 10000 300 0 500 1 0 output/mpi1.txt
-mpirun -np 3 ./mpi_dist_general 10000 300 0 500 1 0 output/mpi2.txt
-mpirun -np 4 ./mpi_dist_general 10000 300 0 500 1 0 output/mpi3.txt
-mpirun -np 5 ./mpi_dist_general 10000 300 0 500 1 0 output/mpi4.txt
-mpirun -np 6 ./mpi_dist_general 10000 300 0 500 1 0 output/mpi5.txt
-mpirun -np 2 ./mpi_dist_general 20000 300 0 500 1 0 output/mpi1.txt
-mpirun -np 3 ./mpi_dist_general 20000 300 0 500 1 0 output/mpi2.txt
-mpirun -np 4 ./mpi_dist_general 20000 300 0 500 1 0 output/mpi3.txt
-mpirun -np 5 ./mpi_dist_general 20000 300 0 500 1 0 output/mpi4.txt
-mpirun -np 6 ./mpi_dist_general 20000 300 0 500 1 0 output/mpi5.txt
-mpirun -np 2 ./mpi_dist_general 50000 300 0 500 1 0 output/mpi1.txt
-mpirun -np 3 ./mpi_dist_general 50000 300 0 500 1 0 output/mpi2.txt
-mpirun -np 4 ./mpi_dist_general 50000 300 0 500 1 0 output/mpi3.txt
-mpirun -np 5 ./mpi_dist_general 50000 300 0 500 1 0 output/mpi4.txt
-mpirun -np 6 ./mpi_dist_general 50000 300 0 500 1 0 output/mpi5.txt
-mpirun -np 2 ./mpi_dist_general 100000 300 0 500 1 0 output/mpi1.txt
-mpirun -np 3 ./mpi_dist_general 100000 300 0 500 1 0 output/mpi2.txt
-mpirun -np 4 ./mpi_dist_general 100000 300 0 500 1 0 output/mpi3.txt
-mpirun -np 5 ./mpi_dist_general 100000 300 0 500 1 0 output/mpi4.txt
-mpirun -np 6 ./mpi_dist_general 100000 300 0 500 1 0 output/mpi5.txt
+./random output_syn/rand.txt -n 2000 -m 100
+./random output_syn/rand.txt -n 2000 -m 200
+./random output_syn/rand.txt -n 2000 -m 500
+./random output_syn/rand.txt -n 2000 -m 1000
+./random output_syn/rand.txt -n 2000 -m 2000
+./random output_syn/rand.txt -n 2000 -m 5000
+./random output_syn/rand.txt -n 2000 -m 10000
+mpirun -np 2 ./mpi_dist_general 2000 100 0 500 1 0 output_syn/mpi1.txt
+mpirun -np 3 ./mpi_dist_general 2000 100 0 500 1 0 output_syn/mpi2.txt
+mpirun -np 4 ./mpi_dist_general 2000 100 0 500 1 0 output_syn/mpi3.txt
+mpirun -np 5 ./mpi_dist_general 2000 100 0 500 1 0 output_syn/mpi4.txt
+mpirun -np 6 ./mpi_dist_general 2000 100 0 500 1 0 output_syn/mpi5.txt
+mpirun -np 2 ./mpi_dist_general 2000 200 0 500 1 0 output_syn/mpi1.txt
+mpirun -np 3 ./mpi_dist_general 2000 200 0 500 1 0 output_syn/mpi2.txt
+mpirun -np 4 ./mpi_dist_general 2000 200 0 500 1 0 output_syn/mpi3.txt
+mpirun -np 5 ./mpi_dist_general 2000 200 0 500 1 0 output_syn/mpi4.txt
+mpirun -np 6 ./mpi_dist_general 2000 200 0 500 1 0 output_syn/mpi5.txt
+mpirun -np 2 ./mpi_dist_general 2000 500 0 500 1 0 output_syn/mpi1.txt
+mpirun -np 3 ./mpi_dist_general 2000 500 0 500 1 0 output_syn/mpi2.txt
+mpirun -np 4 ./mpi_dist_general 2000 500 0 500 1 0 output_syn/mpi3.txt
+mpirun -np 5 ./mpi_dist_general 2000 500 0 500 1 0 output_syn/mpi4.txt
+mpirun -np 6 ./mpi_dist_general 2000 500 0 500 1 0 output_syn/mpi5.txt
+mpirun -np 2 ./mpi_dist_general 2000 1000 0 500 1 0 output_syn/mpi1.txt
+mpirun -np 3 ./mpi_dist_general 2000 1000 0 500 1 0 output_syn/mpi2.txt
+mpirun -np 4 ./mpi_dist_general 2000 1000 0 500 1 0 output_syn/mpi3.txt
+mpirun -np 5 ./mpi_dist_general 2000 1000 0 500 1 0 output_syn/mpi4.txt
+mpirun -np 6 ./mpi_dist_general 2000 1000 0 500 1 0 output_syn/mpi5.txt
+mpirun -np 2 ./mpi_dist_general 2000 2000 0 500 1 0 output_syn/mpi1.txt
+mpirun -np 3 ./mpi_dist_general 2000 2000 0 500 1 0 output_syn/mpi2.txt
+mpirun -np 4 ./mpi_dist_general 2000 2000 0 500 1 0 output_syn/mpi3.txt
+mpirun -np 5 ./mpi_dist_general 2000 2000 0 500 1 0 output_syn/mpi4.txt
+mpirun -np 6 ./mpi_dist_general 2000 2000 0 500 1 0 output_syn/mpi5.txt
+mpirun -np 2 ./mpi_dist_general 2000 5000 0 500 1 0 output_syn/mpi1.txt
+mpirun -np 3 ./mpi_dist_general 2000 5000 0 500 1 0 output_syn/mpi2.txt
+mpirun -np 4 ./mpi_dist_general 2000 5000 0 500 1 0 output_syn/mpi3.txt
+mpirun -np 5 ./mpi_dist_general 2000 5000 0 500 1 0 output_syn/mpi4.txt
+mpirun -np 6 ./mpi_dist_general 2000 5000 0 500 1 0 output_syn/mpi5.txt
+mpirun -np 2 ./mpi_dist_general 2000 10000 0 500 1 0 output_syn/mpi1.txt
+mpirun -np 3 ./mpi_dist_general 2000 10000 0 500 1 0 output_syn/mpi2.txt
+mpirun -np 4 ./mpi_dist_general 2000 10000 0 500 1 0 output_syn/mpi3.txt
+mpirun -np 5 ./mpi_dist_general 2000 10000 0 500 1 0 output_syn/mpi4.txt
+mpirun -np 6 ./mpi_dist_general 2000 10000 0 500 1 0 output_syn/mpi5.txt
